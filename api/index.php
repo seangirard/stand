8e0f986dbdebd9cc5cc89e87ace0d3eba2b91da7
Sean Girard <seangirard@yahoo.com>
2014-07-03 13:58:10 -0700

    api

--- api/index.php
+++ api/index.php
@@ -208,9 +208,9 @@ class SG_STA_GTFS {
 
 		$q = $this->query($sql, $params);
 		foreach ( $q as $k => $time ) {
-			$arrival = new DateTime($time->arrival_time);
+			$arrival = new DateTime('today '.$time->arrival_time);
 			$q[$k]->arrival_time_formatted = $arrival->format('g:m a');
-			$departure = new DateTime($time->departure_time);
+			$departure = new DateTime('today '.$time->departure_time);
 			$q[$k]->departure_time_formatted = $departure->format('g:m a');
 		}
 		return $q;
