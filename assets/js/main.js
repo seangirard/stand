4383af04b96b4b1321bb6390acbce6555e6e1e3a
Sean Girard <seangirard@yahoo.com>
2014-07-02 20:58:04 -0700

    bootstrap

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(function() {
+
+	$('.find-supper').typeahead('destroy');
+
+	
+	$('.find-supper').typeahead([
+	{
+		hint: false,
+		name: 'find-supper',
+		//prefetch: 'search.json',
+		prefetch: {
+			url: 'search.json',
+    	//url: 'http://twitter.github.io/typeahead.js/data/countries.json',
+    	ttl: 1 // in milliseconds
+		},  
+		template: [                                                                 
+        '<a style="" href="{{href}}">{{title}}</a>'
+    ].join(''),                                                                 
+    engine: Hogan
+	}
+	]);
+
+	$('.find-supper').on('typeahead:selected', function (e, datum) {
+    //console.log(datum);
+    window.location.href = datum.href;
+	});
+
+});
